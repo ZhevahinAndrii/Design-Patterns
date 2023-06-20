551200aaf1255a9b490500fbf9bb7a6c403d4f55
shtrih2k4 <andriizhevahin@gmail.com>
2023-06-20 10:50:01 +0300

    Добавьте файлы проекта.

---
+++ Decorator/Program.cs
@@ -0,0 +1,86 @@
+﻿public class Decorator
+{
+    static void Main(string[] args)
+    {
+        Pizza pizza1 = new ItalianPizza();
+        pizza1 = new TomatoPizza(pizza1); 
+        Console.WriteLine("Name: {0}", pizza1.Name);
+        Console.WriteLine("Cost: {0}", pizza1.GetCost());
+
+        Pizza pizza2 = new ItalianPizza();
+        pizza2 = new CheesePizza(pizza2);
+        Console.WriteLine("Name: {0}", pizza2.Name);
+        Console.WriteLine("Cost: {0}", pizza2.GetCost());
+
+        Pizza pizza3 = new BulgarianPizza();
+        pizza3 = new TomatoPizza(pizza3);
+        pizza3 = new CheesePizza(pizza3);/
+        Console.WriteLine("Name: {0}", pizza3.Name);
+        Console.WriteLine("Cost: {0}", pizza3.GetCost());
+
+        
+    }
+}
+
+abstract class Pizza
+{
+    public Pizza(string n)
+    {
+        Name = n;
+    }
+    public string Name { get; protected set; }
+    public abstract int GetCost();
+}
+
+class ItalianPizza : Pizza
+{
+    public ItalianPizza() : base("Italian pizza")
+    { }
+    public override int GetCost()
+    {
+        return 10;
+    }
+}
+class BulgarianPizza : Pizza
+{
+    public BulgarianPizza()
+        : base("Bulgarian Pizza")
+    { }
+    public override int GetCost()
+    {
+        return 8;
+    }
+}
+
+abstract class PizzaDecorator : Pizza
+{
+    protected Pizza pizza;
+    public PizzaDecorator(string n, Pizza pizza) : base(n)
+    {
+        this.pizza = pizza;
+    }
+}
+
+class TomatoPizza : PizzaDecorator
+{
+    public TomatoPizza(Pizza p)
+        : base(p.Name + ", with tomatoes", p)
+    { }
+
+    public override int GetCost()
+    {
+        return pizza.GetCost() + 3;
+    }
+}
+
+class CheesePizza : PizzaDecorator
+{
+    public CheesePizza(Pizza p)
+        : base(p.Name + ", with cheese", p)
+    { }
+
+    public override int GetCost()
+    {
+        return pizza.GetCost() + 5;
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,73 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Hero hero = new Hero();
+        hero.Shoot(); // стріляємо, залишилось 9 патронів
+        GameHistory game = new GameHistory();
+
+        game.History.Push(hero.SaveState()); // зберігаємо гру
+
+        hero.Shoot(); //стріляємо, залишилось 8 патронів
+
+        hero.RestoreState(game.History.Pop());
+
+        hero.Shoot(); //стріляємо , залишилось 8 патронів
+
+      
+    }
+}
+
+// Originator
+class Hero
+{
+    private int patrons = 10; // amount of bulles
+    private int lives = 5; // amount of lives
+
+    public void Shoot()
+    {
+        if (patrons > 0)
+        {
+            patrons--;
+            Console.WriteLine("Shoots.{0} bullets left", patrons);
+        }
+        else
+            Console.WriteLine("There are no bullets anymore");
+    }
+    // сохранение состояния
+    public HeroMemento SaveState()
+    {
+        Console.WriteLine("Saving. Parameters: {0} bullets, {1} lives", patrons, lives);
+        return new HeroMemento(patrons, lives);
+    }
+
+    // восстановление состояния
+    public void RestoreState(HeroMemento memento)
+    {
+        patrons = memento.Patrons;
+        lives = memento.Lives;
+        Console.WriteLine("Restoring. Parameters: {0} bullets, {1} lives", patrons, lives);
+    }
+}
+// Memento
+class HeroMemento
+{
+    public int Patrons { get; private set; }
+    public int Lives { get; private set; }
+
+    public HeroMemento(int patrons, int lives)
+    {
+        Patrons = patrons;
+        Lives = lives;
+    }
+}
+
+// Caretaker
+class GameHistory
+{
+    public Stack<HeroMemento> History { get; private set; }
+    public GameHistory()
+    {
+        History = new Stack<HeroMemento>();
+    }
+}
